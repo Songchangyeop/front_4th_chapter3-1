6a7110a2376588579a5ba51ffbdbeed6f7ea1ef8
Changyeop Song <waitwait0301@naver.com>
2025-02-04 00:21:05 +0900

    feat: notificationUrils.spec 테스트 코드 추가

--- src/__tests__/unit/easy.notificationUtils.spec.ts
+++ src/__tests__/unit/easy.notificationUtils.spec.ts
@@ -1,16 +1,65 @@
 import { Event } from '../../types';
 import { createNotificationMessage, getUpcomingEvents } from '../../utils/notificationUtils';
 
+const events: Event[] = [
+  {
+    id: '1',
+    title: '기존 회의',
+    date: '2024-07-04',
+    startTime: '09:00',
+    endTime: '10:00',
+    description: '기존 팀 미팅',
+    location: '회의실 B',
+    category: '업무',
+    repeat: { type: 'none', interval: 0 },
+    notificationTime: 10,
+  },
+  {
+    id: '2',
+    title: '이전 회의',
+    date: '2024-07-01',
+    startTime: '10:00',
+    endTime: '12:00',
+    description: '기존 팀 미팅',
+    location: '회의실 A',
+    category: '업무',
+    repeat: { type: 'none', interval: 0 },
+    notificationTime: 10,
+  },
+];
+
 describe('getUpcomingEvents', () => {
-  it('알림 시간이 정확히 도래한 이벤트를 반환한다', () => {});
+  it('알림 시간이 정확히 도래한 이벤트를 반환한다', () => {
+    const result = getUpcomingEvents(events, new Date('2024-07-04T08:50'), []);
 
-  it('이미 알림이 간 이벤트는 제외한다', () => {});
+    expect(result).toHaveLength(1);
 
-  it('알림 시간이 아직 도래하지 않은 이벤트는 반환하지 않는다', () => {});
+    expect(result[0]).toEqual(events[0]);
+  });
 
-  it('알림 시간이 지난 이벤트는 반환하지 않는다', () => {});
+  it('이미 알림이 간 이벤트는 제외한다', () => {
+    const result = getUpcomingEvents(events, new Date('2024-07-04T08:50'), ['1']);
+
+    expect(result).toHaveLength(0);
+  });
+
+  it('알림 시간이 아직 도래하지 않은 이벤트는 반환하지 않는다', () => {
+    const result = getUpcomingEvents(events, new Date('2024-07-04T08:40'), []);
+
+    expect(result).toHaveLength(0);
+  });
+
+  it('알림 시간이 지난 이벤트는 반환하지 않는다', () => {
+    const result = getUpcomingEvents(events, new Date('2024-07-04T09:10'), []);
+
+    expect(result).toHaveLength(0);
+  });
 });
 
 describe('createNotificationMessage', () => {
-  it('올바른 알림 메시지를 생성해야 한다', () => {});
+  it('올바른 알림 메시지를 생성해야 한다', () => {
+    const result = createNotificationMessage(events[0]);
+
+    expect(result).toBe('10분 후 기존 회의 일정이 시작됩니다.');
+  });
 });
